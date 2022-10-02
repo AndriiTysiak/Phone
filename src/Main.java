9ae3f2db7e815fe2b89d7e5ebdbc31ccde28d3ab
Andrii Tysiak <77442257+AndriiTysiak@users.noreply.github.com>
2022-10-02 17:22:45 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Phone phone1 = new Phone(1111,"Iphone",1);
+        Phone phone2 = new Phone();
+        Phone phone3 = new Phone();
+        System.out.println(phone1);
+    }
+}
---
+++ src/Phone.java
@@ -0,0 +1,23 @@
+public class Phone {
+    int number;
+    String model;
+    double weight;
+
+    public Phone(int number, String model, double weight) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+    public Phone(){
+
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "number=" + number +
+                ", model='" + model + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
